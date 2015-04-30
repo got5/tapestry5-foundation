6fa9f8239a182aea68f0f9b3e799a7b5607bc3b3
ffacon <francois.facon@gmail.com>
2015-04-30 22:45:26 +0200

    Add Accordion sample

---
+++ src/test/java/org/got5/tapestry5/jquery/pages/Accordion.java
@@ -0,0 +1,15 @@
+package org.got5.tapestry5.jquery.pages;
+
+import org.apache.tapestry5.json.JSONObject;
+
+
+public class Accordion {
+
+	public JSONObject getParams(){
+		JSONObject json =  new JSONObject();
+		json.put("active_class", "active");// specify the class used for active sections
+		json.put("toggleable",true);
+        return json;
+	}
+	
+}
